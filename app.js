fb62ca19140bf8df8009739c9459ae8d1ff626b3
Yao-Hsien Hsieh <laxative0612@gmail.com>
2018-11-06 22:49:12 +0800

    [Package init] npm init
    Environment setting: import babel and webpack

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname + "/dist")));
+
+
+// setting server
+var server = app.listen(port, () => {
+    console.log(`server now listen at ${port} port.`)
+})
---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+        usedExports: true
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(.woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
+
