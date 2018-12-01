45438ce2c32bd09efa49157ecc820872725fc7ba
Li Chengxue <930leo@gmail.com>
2018-12-01 20:14:40 +0800

    The touchlayer work

--- src/mainMap/mainMap.js
+++ src/mainMap/mainMap.js
@@ -1,90 +1,139 @@
-var t;
 
-const chunkNum = 2;
-const spaceNum = 16;
-const size = 900;
-class Space {
-  constructor(row, col) {
-    this.isOccupied = false;
-    this.row = row;
-    this.col = col;
-    this.position = new PIXI.Point(col * size / chunkNum / spaceNum, row * size / chunkNum / spaceNum);
-    //this.touch = touch;
+//const size = 900;
+//const totalSpacesNum = 16 * 2;
+
+function createTouchLayer(width, height, buildingSizeX, buildingSizeY) {
+  const chunkNum = 1;
+  const spaceNum = 32;
+  class Space {
+    constructor(row, col) {
+      this.isOccupied = false;
+      this.row = row;
+      this.col = col;
+      this.position = new PIXI.Point(col * width / chunkNum / spaceNum, row * height / chunkNum / spaceNum);
+      //this.touch = touch;
+    }
   }
-}
-class Trunk {
-  constructor(index, container) {
-    this.index = index;
-    this.container = container;
-    let x = (index % chunkNum) * size / chunkNum;
-    let y = Math.floor(index / chunkNum) * size / chunkNum;
-    container.position.set(x, y);
-    this.spaceArr = new Array(spaceNum);
-    for(let i = 0; i < spaceNum; i++) {
-      this.spaceArr[i] = new Array(spaceNum);
-      for(let j = 0; j < spaceNum; j++) {
-        this.spaceArr[i][j] = new Space(i, j);
+  class Chunk {
+    constructor(index, container) {
+      this.index = index;
+      this.container = container;
+      let x = (index % chunkNum) * width / chunkNum;
+      let y = Math.floor(index / chunkNum) * height / chunkNum;
+      container.position.set(x, y);
+      this.spaceArr = new Array(spaceNum);
+      for(let i = 0; i < spaceNum; i++) {
+        this.spaceArr[i] = new Array(spaceNum);
+        for(let j = 0; j < spaceNum; j++) {
+          this.spaceArr[i][j] = new Space(i, j);
+        }
       }
     }
   }
-}
-function showTouchLayer(trunkArr) {
-  let touchLayer = new Array();
-  for(let i = 0; i < trunkArr.length; i++) {
-    for(let j = 0; j < trunkArr[i].spaceArr.length; j++) {
-      for(let k = 0; k < trunkArr[i].spaceArr[j].length; k++) {
+  let container;
+  let chunkArr = new Array(Math.pow(chunkNum, 2));
+  for(let i = 0; i < Math.pow(chunkNum, 2); i++) {
+    container = new PIXI.Container();
+    chunkArr[i] = new Chunk(i, container);
+  }
+  let touchLayer = new Array(chunkNum * chunkNum);
+  for(let i = 0; i < chunkArr.length; i++) {
+    touchLayer[i] = new Array(spaceNum);
+    for(let j = 0; j < chunkArr[i].spaceArr.length; j++) {
+      touchLayer[i][j] = new Array(spaceNum);
+      for(let k = 0; k < chunkArr[i].spaceArr[j].length; k++) {
         let touch = new PIXI.Graphics();
+        touchLayer[i][j][k] = touch;
         touch.tag = 1;
-        let spaceSize = size / chunkNum / spaceNum;
-        trunkArr[i].container.addChild(touch);
-        touchLayer.push(touch);
+        let spaceSizeX = width / chunkNum / spaceNum;
+        let spaceSizeY = height / chunkNum / spaceNum;
+        chunkArr[i].container.addChild(touch);
+        //touchLayer.push(touch);
         touch.beginFill(0xFFFFFF);
+        touch.alpha = 0.5;
         touch.lineStyle(1, 0x000000);
-        touch.drawRect(0, 0, spaceSize, spaceSize);
+        touch.drawRect(0, 0, spaceSizeX, spaceSizeY);
         touch.endFill();
         touch.interactive = true;
-        touch.hitArea = new PIXI.Circle(0, 0, spaceSize, spaceSize);
-        touch.position = trunkArr[i].spaceArr[j][k].position.clone();
-        this.alpha = 1;
+        touch.hitArea = new PIXI.Rectangle(0, 0, spaceSizeX, spaceSizeY);
+        touch.position = chunkArr[i].spaceArr[j][k].position.clone();
+
+        let count = 0;
         touch.mouseover = function(mouseData) {
-          this.alpha = 0.5;
+          count++;
+          for(let m = 0; m < spaceNum; m++) {
+            for(let n = 0; n < spaceNum; n++) {
+              if(m >= j && m < j + buildingSizeY && n >= k && n < k + buildingSizeX) {
+                touchLayer[i][m][n].alpha = 0.2;
+              }
+              else {
+                touchLayer[i][m][n].alpha = 0.5;
+              }
+            }
+          }
         }
+        /*
         touch.mouseout = function(mouseData) {
-          this.alpha = 1;
+          count--;
+          if(count == 0) {
+            for(let m = 0; m < spaceNum; m++) {
+              for(let n = 0; n < spaceNum; n++) {
+                touchLayer[i][m][n].alpha = 1;
+              }
+            }
+          }
         }
+        */
       }
     }
   }
-  return touchLayer;
+  /*
+  container.reset = function() {
+  }
+  container.show = function(parent) {
+    this.reset();
+    parent.addChild(this);
+  }
+  container.hide = function() {
+    this.parent.removeChild(this);
+  }
+  */
+  return container;
 }
+/*
 function hideTouchLayer(touchLayer) {
   for(let i = 0; i < touchLayer.length; i++) {
-    touchLayer[i].parent.removeChild(touchLayer[i]);
-    touchLayer[i].destroy({children:true, texture:true, baseTexture:true});
+    for(let j = 0; j < touchLayer[i].length; j++) {
+      for(let k = 0; k < touchLayer[i][j].length; k++) {
+        touchLayer[i][j][k].parent.removeChild(touchLayer[i][j][k]);
+        touchLayer[i][j][k].destroy({children:true, texture:true, baseTexture:true});
+      }
+    }
   }
 }
-const app = new PIXI.Application({width: size, height: size});
+*/
+const app = new PIXI.Application({width: 900, height: 900});
 document.body.appendChild(app.view);
 PIXI.loader.add("img/cat.png").load(setup);
 function setup() {
   let map = new PIXI.Container();
-  let texture = PIXI.loader.resources["img/cat.png"].texture;
-  let trunkArr = new Array(Math.pow(chunkNum, 2));
-  for(let i = 0; i < Math.pow(chunkNum, 2); i++) {
-    let container = new PIXI.Container();
-    app.stage.addChild(container);
-    trunkArr[i] = new Trunk(i, container);
-
-  }
+  //let texture = PIXI.loader.resources["img/cat.png"].texture;
+  buildingSizeX = 3;
+  buildingSizeY = 2;
+  let c;
   document.onkeydown = function(event) {
     var keycode = event.which || event.keyCode;
     if(keycode == 65){
-      t = showTouchLayer(trunkArr);
+      //c.show(app.stage);
+      c = createTouchLayer(700, 500, buildingSizeX, buildingSizeY);
+      app.stage.addChild(c);
       //alert("A");
     }
     if(keycode == 83){
+      //c.hide();
+      app.stage.removeChild(c);
       //alert("S");
-      hideTouchLayer(t);
+      //hideTouchLayer(t);
     }
   }
   //showTouchLayer(trunkArr);
