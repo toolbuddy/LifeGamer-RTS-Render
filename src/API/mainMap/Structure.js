859fc49bf34196813d666e874022287f5faf0994
Yao-Hsien Hsieh <laxative0612@gmail.com>
2018-12-02 22:29:04 +0800

    Modify structure data

---
+++ src/API/mainMap/Structure.js
@@ -0,0 +1,175 @@
+var structures = {
+    'ThermalPowerPlant': {
+        "ID" : 1,
+        "Name" : "Thermal Power Plant",
+        "Terrain" : [1,2,4,32,64,128,512],
+        "Cost" : 10000,
+        "Power" : 1000,
+        "Human" : -10,
+        "Money" : -1500,
+        "Size" : 4
+    },
+    'WaterPowerPlant': {
+            "ID" : 2,
+            "Name" : "Water Power Plant",
+            "Terrain" : [16,128],
+            "Cost" : 15000,
+            "Power" : 750,
+            "Human" : -5,
+            "Money" : -500,
+            "Size" : 4
+    },
+    'WindPowerPlant': {
+        "ID" : 3,
+        "Name" : "Wind Power Plant",
+        "Terrain" : [2,64],
+        "Cost" : 15000,
+        "Power" : 250,
+        "Human" : -1,
+        "Money" : -300,
+        "Size" : 4
+    },
+    'TidalPowerPlant': {
+        "ID" : 4,
+        "Name" : "Tidal Power Plant",
+        "Terrain" : [8,64],
+        "Cost" : 10000,
+        "Power" : 500,
+        "Human" : -2,
+        "Money" : -500,
+        "Size" : 4
+    },
+    'SolarPowerPlant': {
+        "ID" : 5,
+        "Name" : "Solar Power Plant",
+        "Terrain" : [1],
+        "Cost" : 20000,
+        "Power" : 500,
+        "Human" : -1,
+        "Money" : -200,
+        "Size" : 4
+    },
+    'GeoThermalPowerPlant': {
+        "ID" : 6,
+        "Name" : "GeoThermal Power Plant",
+        "Terrain" : [512],
+        "Cost" : 20000,
+        "Power" : 500,
+        "Human" : -1,
+        "Money" : -300,
+        "Size" : 4
+    },
+    'NuclearPowerPlant': {
+        "ID" : 7,
+        "Name" : "Nuclear Power Plant",
+        "Terrain" : [64],
+        "Cost" : 70000,
+        "Power" : 3000,
+        "Human" : -20,
+        "Money" : -6000,
+        "Size" : 8
+    },
+    'BitCoinMiner': {
+        "ID" : 10,
+        "Name" : "BitCoin Miner",
+        "Terrain" : [1,2,4,32,64,128,512],
+        "Cost" : 5000,
+        "Power" : -2000,
+        "Human" : -1,
+        "Money" : 5000,
+        "Size" : 2
+    },
+    'Sawmill': {
+        "ID" : 11,
+        "Name" : "Sawmill",
+        "Terrain" : [4],
+        "Cost" : 8000,
+        "Power" : -500,
+        "Human" : -10,
+        "Money" : 2000,
+        "Size" : 4
+    },
+    'FishFarm': {
+        "ID" : 12,
+        "Name" : "Fish Farm",
+        "Terrain" : [8,16,64,128],
+        "Cost" : 12000,
+        "Power" : -200,
+        "Human" : -5,
+        "Money" : 2000,
+        "Size" : 4
+    },
+    'ICFab': {
+        "ID" : 13,
+        "Name" : "IC Fab",
+        "Terrain" : [1],
+        "Cost" : 60000,
+        "Power" : -2500,
+        "Human" : -20,
+        "Money" : 7000,
+        "Size" : 4
+    },
+    'Pasture': {
+        "ID" : 14,
+        "Name" : "Pasture",
+        "Terrain" : [2],
+        "Cost" : 8000,
+        "Power" : -100,
+        "Human" : -30,
+        "Money" : 2000,
+        "Size" : 4
+    },
+    'Hotspring': {
+        "ID" : 15,
+        "Name" : "Hotspring",
+        "Terrain" : [512],
+        "Cost" : 0,
+        "Power" : 0,
+        "Human" : -2,
+        "Money" : 1000,
+        "Size" : 4
+    },
+    'SmallMilitaryCamp': {
+        "ID" : 21,
+        "Name" : "Military Camp (small)",
+        "Terrain" : [1,2,4,32,64,128],
+        "Cost" : 10000,
+        "Power" : -200,
+        "Human" : 0,
+        "Money" : 0,
+        "Size" : 2
+    },
+    'MediumMilitaryCamp': {
+        "ID" : 22,
+        "Name" : "Military Camp (medium)",
+        "Terrain" : [1,2,4,32,64,128],
+        "Cost" : 25000,
+        "Power" : -400,
+        "Human" : 0,
+        "Money" : 0,
+        "Size" : 4
+    },
+    'LargeMilitaryCamp': {
+        "ID" : 23,
+        "Name" : "Military Camp (large)",
+        "Terrain" : [1,2,4,32,64,128],
+        "Cost" : 60000,
+        "Power" : -500,
+        "Human" : 0,
+        "Money" : 0,
+        "Size" : 8
+    },
+    'Observatory': {
+        "ID" : 24,
+        "Name" : "Observatory",
+        "Terrain" : [1,2,4,32,64,128,512],
+        "Cost" : 8000,
+        "Power" : -100,
+        "Human" : -2,
+        "Money" : -500,
+        "Size" : 2
+    }
+}
+
+export default structures
+
