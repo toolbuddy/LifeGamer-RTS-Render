7a89acb5def7b37d2315efa4dc0579a148122029
Yao-Hsien Hsieh <laxative0612@gmail.com>
2018-12-05 06:21:05 +0800

    Fix bug & Implement CreateBuildLayer function
    - Fix bug: CalcAllowBuildPoint calculating error
    - CreateBuildLayer: The function return a container contains
    background and select space, the select space will follow mouse
    and shows color
        - green: means user can build in here
        - red: means user cannot build
    
    After clicking the place, it'll send build oper request to backend
    server.

---
+++ src/mainMap/CreateBuildLayer.js
@@ -0,0 +1,91 @@
+import Structures from '../API/mainMap/Structure'
+
+import * as PIXI from 'pixi.js'
+import * as API from '../API'
+
+const layerSize = 768
+const chunkCoor = 2
+const spaceCoor = 16
+const spaceSize = layerSize / chunkCoor / spaceCoor
+
+var allowPoints = null
+var mapData = null
+
+export default function CreateBuildLayer (conn, MapData, building) {
+    return new Promise(async resolve => {
+        // create layer
+        var layer = new PIXI.Container()
+        // create background
+        var background = await backgroundCreate()
+        layer.addChild(background)
+        // select space create
+        layer.selectSpace = await selectSpaceCreate(building)
+
+        allowPoints = await API.mainMap.CalcAllowBuildPoint(MapData, building)
+        mapData = MapData
+
+        // mouse event setting
+        layer.interactive = true
+
+        layer.mouseover = function(mouseData) {
+            console.log(mouseData.currentTarget)
+            let chunkIndex = Math.floor(mouseData.data.global.x / (layerSize / chunkCoor)) + Math.floor(mouseData.data.global.y / (layerSize / chunkCoor)) * chunkCoor
+            let index = Math.floor(mouseData.data.global.x / spaceSize) % spaceCoor + Math.floor(mouseData.data.global.y / spaceSize) % spaceCoor * spaceCoor
+            this.isHover = true
+            this.addChild(this.selectSpace)
+            this.selectSpace.x = Math.floor(mouseData.data.global.x / spaceSize) * spaceSize
+            this.selectSpace.y = Math.floor(mouseData.data.global.y / spaceSize) * spaceSize
+            this.selectSpace.graphicsData[0].fillColor = (allowPoints[chunkIndex][index]) ? 0x00ff00 : 0xff0000
+        }
+
+        layer.mousemove = function(mouseData) {
+            if(this.isHover) {
+                let chunkIndex = Math.floor(mouseData.data.global.x / (layerSize / chunkCoor)) + Math.floor(mouseData.data.global.y / (layerSize / chunkCoor)) * chunkCoor
+                let index = Math.floor(mouseData.data.global.x / spaceSize) % spaceCoor + Math.floor(mouseData.data.global.y / spaceSize) % spaceCoor * spaceCoor
+                this.selectSpace.x = Math.floor(mouseData.data.global.x / spaceSize) * spaceSize
+                this.selectSpace.y = Math.floor(mouseData.data.global.y / spaceSize) * spaceSize
+                this.selectSpace.graphicsData[0].fillColor = (allowPoints[chunkIndex][index]) ? 0x00ff00 : 0xff0000
+            }
+        }
+
+        layer.mouseout = function(mouseData) {
+            this.isHover = false
+            this.removeChild(this.selectSpace)
+        }
+
+        layer.on('pointerdown', function(mouseData) {
+            let chunkIndex = Math.floor(mouseData.data.global.x / (layerSize / chunkCoor)) + Math.floor(mouseData.data.global.y / (layerSize / chunkCoor)) * chunkCoor
+            let x = Math.floor(mouseData.data.global.x / spaceSize) % spaceCoor
+            let y = Math.floor(mouseData.data.global.y / spaceSize) % spaceCoor
+            API.mainMap.BuildOperRequest(conn, 'Build', Structures[building].ID, mapData[chunkIndex].Pos, {'X': x, 'Y': y})
+            API.miniMap.ViewRangeMapdataRequest(conn, {'X': 0, 'Y': 0})
+        })
+
+        layer.zIndex = 99
+        layer.alpha = 0.5
+        resolve(layer)
+    })
+}
+
+function backgroundCreate() {
+    return new Promise(resolve => {
+        var bg = new PIXI.Graphics()
+        bg.beginFill(0x000000)
+        bg.drawRect(0, 0, layerSize, layerSize)
+        bg.endFill()
+
+        resolve(bg)
+    })
+}
+
+
+function selectSpaceCreate (building) {
+    return new Promise(resolve => {
+        var selectSpace = new PIXI.Graphics()
+        selectSpace.beginFill(0x00ff00)
+        selectSpace.drawRect(0, 0, Structures[building].Size * spaceSize, Structures[building].Size * spaceSize)
+        selectSpace.endFill()
+
+        resolve(selectSpace)
+    })
+}
