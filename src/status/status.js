f2e8b354a31bb92c3f8f2c01367a0cde26df8579
Chiaki <linmichael40815@gmail.com>
2018-12-16 17:56:57 +0800

    revise the thousands dot regular expression

--- src/status/status.js
+++ src/status/status.js
@@ -1,6 +1,6 @@
 import statusCSS from './style.css' 
 
-const UpperLimit = 9999999;
+const UpperLimit = 999999;
 const p_Power = document.querySelector("#Power .Data p");
 const p_People = document.querySelector("#People .Data p");
 const p_Money = document.querySelector("#Money .Data p");
@@ -14,15 +14,27 @@ const p_Money = document.querySelector("#Money .Data p");
  * @param {string | int} Money - the value of Money
  */
 function writeData(Power, People, Money){
+    p_Power.textContent = thousandDots(numberJudge(Power));
+    p_People.textContent = thousandDots(numberJudge(People));
+    p_Money.textContent = thousandDots(numberJudge(Money));
+}
 
-    Power = (Power>UpperLimit)? UpperLimit : Power;
-    People = (People>UpperLimit)? UpperLimit : People;
-    Money = (Money>UpperLimit)? UpperLimit : Money;
+function numberJudge(str){
+	if(str.split("/")[1]==null){
+		return (str>UpperLimit)? thousandToK(str):str;
+	}else{
+		return ((str.split("/")[0]>UpperLimit)? thousandToK(str.split("/")[0]):str.split("/")[0]) + "/" +
+				((str.split("/")[1]>UpperLimit)? thousandToK(str.split("/")[1]):str.split("/")[1]) ;
+	}
+}
 
-    p_Power.textContent = Power.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
-    p_People.textContent = People.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
-    p_Money.textContent = Money.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+function thousandToK(number){
+	return number.slice(0,-3).concat("k");
+}
+
+function thousandDots(number){
+	return number.toString().replace(/(\d{1,3})(?=(\d{3})+\k)/g, "$1,")
 }
 
 // The test for render
-writeData("123456789", 456, 12345678)
+writeData("2000/3000000", "4007/1000000", "1234567")
